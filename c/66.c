021d37e268741a73b9073f8a5b8a614c50c46f83
junfan <qianjunfan0@outlook.com>
2026-04-16 09:18:29 +0000

    new

--- c/66.c
+++ c/66.c
@@ -2,9 +2,26 @@
  * Apr 16, 2026
  */
 
+#include <stdlib.h>
+
 int *plusOne(int *d, int size, int *rsize)
 {
+	for (int i = size - 1; i >= 0; --i) {
+		if (d[i] < 9) {
+			++d[i];
+			*rsize = size;
 
+			return d;
+		}
 
+		d[i] = 0;
+	}
 
+	int *result = malloc((size + 1) * sizeof(int));
+	result[0] = 1;
+	for (int i = 1; i <= size; ++i)
+		result[i] = 0;
+
+	*rsize = size + 1;
+	return result;
 }

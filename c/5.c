13b6aea38830990913b034b0fe60ac25574beccf
junfan <qianjunfan0@outlook.com>
2026-04-24 08:15:58 +0000

    new

--- c/5.c
+++ c/5.c
@@ -2,10 +2,11 @@
  * Apr 24, 2026
  */
 
-
-
-/* A Center-spread Brute-force Solution */
-char *longestPalindrome(char *s)
+/* A Center-spread Brute-force Solution
+ * This is a simple implementation of the center-spread method.
+ * It's slow but effective.
+ */
+char *longestPalindrome_bf(char *s)
 {
         if (*(s + 1) == '\0')
                 return s;
@@ -46,4 +47,28 @@ char *longestPalindrome(char *s)
 
         s[start + max] = '\0';
         return &s[start];
+}
+
+/* Manacher's Algorithm */
+#include <string.h>
+#include <stdlib.h>
+
+char *longestPalindrome(char *s)
+{
+        int n = strlen (s);
+        if (n == 0 && n == 1)
+                return s;
+
+        int len = 2 * n + 3;
+        char *arr = malloc(len * sizeof(char));
+        arr[0] = '^';
+        arr[1] = '#';
+
+        for (int i = 0; i < n; ++i) {
+                arr[2 * i + 2] = s[i];
+                arr[2 * i + 3] = '#';
+        }
+
+        arr[len - 2] = '$';
+        arr[len - 1] = '\0';
 }
